584d4ff7556256520623af3e877078258e187720
sebastiant <jtrujillo2622@gmail.com>
2023-08-30 09:53:57 -0500

    Tarea modulo 4

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,36 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var Suma = x + y;
+   return Suma;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   const Resta = x - y;
+   return Resta;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   const divide = x / y;
+   return divide;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   const multiplica = x * y;
+   return multiplica;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   const obtenerResto = x % y;
+   return obtenerResto;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
